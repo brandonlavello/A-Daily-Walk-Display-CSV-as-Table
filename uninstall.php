a88be2f31e9a56d9998321d7d987b162417f3a6f
Brandon Lavello <blavello@gmail.com>
2025-07-02 20:20:21 -0700

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,4 +1,17 @@
-<?php // exit if uninstall constant is not defined
+<?php
+// Exit if uninstall not called from WordPress
 if (!defined('WP_UNINSTALL_PLUGIN')) exit;
 
+// Remove old option (if it still exists)
 delete_option('adw_csv_id');
+
+// Delete all radio_station posts
+$radio_posts = get_posts([
+    'post_type' => 'radio_station',
+    'numberposts' => -1,
+    'post_status' => 'any',
+]);
+
+foreach ($radio_posts as $post) {
+    wp_delete_post($post->ID, true);
+}
